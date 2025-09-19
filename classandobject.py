55dfbfaf41d0fe41d86d9e3f0635bc3c97cdb306
Sivareddy <peramsivareddy001@gmail.com>
2025-09-19 11:37:47 +0530

    Added code with class and object using parametrized constructor

---
+++ classandobject.py
@@ -0,0 +1,7 @@
+class Abbayi:
+    def __init__(self, name, age):   # parameterized constructor
+        self.name = name
+        self.age = age
+
+p1 = Abbayi("Siva", 21)
+print(f'Abbayi name: {p1.name}\nAbbayi age: {p1.age}')

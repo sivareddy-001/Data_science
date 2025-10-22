ba434e0b2dabc5a9715344789ac8652c49ac4b63
Sivareddy <peramsivareddy001@gmail.com>
2025-10-22 11:54:12 +0530

    A simple inheritance program

---
+++ Inheritance.py
@@ -0,0 +1,13 @@
+# simple code to demonstrate inheritance in Python
+
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+
+class Dog(Animal):
+    def bark(self):
+        print("Dog barks")
+
+dog = Dog()
+dog.speak()
+dog.bark()   
